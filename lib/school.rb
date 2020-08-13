fec89b76186a4a0bed06fface4eff4ec09945d1d
Kordell Parrish <parrishkordell@gmail.com>
2020-08-13 23:28:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,8 +14,7 @@ class School
   def grade(student_grade)
     roster[student_grade]
   end
-
-  # this method should arrange the students in each grade by alphabetical order
+  
   def sort
     sorted = {}
     roster.each do |grade, students|
